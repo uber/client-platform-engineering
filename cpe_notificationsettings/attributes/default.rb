afe627939b67715d75ba2893867323fc034d8581
Erik Gomez <erikg@uber.com>
2019-08-22 16:02:41 -0500

    add cpe_usernotifications

---
+++ cpe_notificationsettings/attributes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: cpe_notificationsettings
+# Attributes:: default
+#
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+#
+# Copyright (c) 2019-present, Uber Technologies, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+#
+
+default['cpe_notificationsettings'] = {
+  'NotificationSettings' => [],
+}
