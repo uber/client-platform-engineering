c1f051df6325a41974fe5517e6d872062eaf075d
Erik Gomez <erikg@uber.com>
2019-10-23 11:55:31 -0500

    add cpe_workspaceone

---
+++ cpe_workspaceone/attributes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: cpe_workspaceone
+# Attributes:: default
+#
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+#
+# Copyright (c) 2019-present, Uber Technologies, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the Apache 2.0 license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+default['cpe_workspaceone'] = {
+  'cache_invalidation' => 7200, # seconds, 2 hours
+  'hubcli_path' => '/Applications/Workspace ONE Intelligent Hub.app/Contents/Resources/'\
+  'IntelligentHubAgent.app/Contents/Resources/cli/hubcli',
+  'install' => false,
+  'manage' => false,
+  'mdm_profiles' => {
+    'enforce' => false,
+    'profiles' => {
+      'device' => [],
+      'user' => [],
+    },
+  },
+  'pkg' => {
+    'allow_downgrade' => false,
+    'app_name' => 'workspace_one_intelligent_hub',
+    'checksum' => nil,
+    'pkg_name' => nil,
+    'pkg_url' => nil,
+    'receipt' => 'com.air-watch.pkg.OSXAgent',
+    'version' => nil,
+  },
+  'prefs' => {
+    'HubAgentIconVisiblePreference' => nil,
+  },
+  'uninstall' => false,
+  'use_cache' => true,
+}
